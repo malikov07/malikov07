87eb935ba2974fb6390059511033e79ff2c74b14
malikov07 <125819333+malikov07@users.noreply.github.com>
2023-02-22 23:05:18 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+
+let options = {
+  strings: ['malikov bekzod dilmurod og\'li ','angren shahar ixtisoslashtirilgan maktab o\'quvchisi'],
+  typeSpeed: 60,
+  startDelay: 1000,
+  backSpeed: 60,
+  loop: true,
+  loopCount: Infinity,
+  
+};
+
+let typed = new Typed('.element', options);
